1bf3f1deb633d931d14c340589e5928b903830be
skysojiro <skysojiro@gmail.com>
2021-10-05 18:48:31 +0900

    URL> http://sign.nhitlab.com:3000 or 3030

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
     // mongoURI: "mongodb://mongo/nhsign",
     mongoURI: "mongodb://localhost/nhsign",
-    // mongoURI: "mongodb://nhit:nacf1234!@face.nhitlab.com:27017/nhsign?authSource=admin",
+    // mongoURI: "mongodb://nhit:nacf1234!@sign.nhitlab.com:27017/nhsign?authSource=admin",
     storageDIR: "storage/"
 }
