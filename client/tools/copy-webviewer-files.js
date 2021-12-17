6de589baba1a7d7ca1a382e123a994ab569a6160
niceharu <3thzone@gmail.com>
2021-12-17 15:56:10 +0900

    pdftron 한국어 배포시 포함

--- client/tools/copy-webviewer-files.js
+++ client/tools/copy-webviewer-files.js
@@ -4,7 +4,7 @@ const copyFiles = async () => {
   try {
     await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer');
     await fs.copy('./pdftron/PDFworker.js', './public/webviewer/core/pdf/PDFworker.js');
-    await fs.copy('./public/webviewer/ui/i18n/translation-ko.json', './public/webviewer/ui/i18n/translation-ko.json');
+    await fs.copy('./pdftron/translation-ko.json', './public/webviewer/ui/i18n/translation-ko.json');
     console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
