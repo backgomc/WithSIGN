2663b8347ca48da55a63efbc752a6c5ea388c6fa
NHIT_LSW <skysojiro@gmail.com>
2021-12-28 19:53:13 +0900

    openapi jsonp 형태 수정

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const config = require('../config/key');
 const restful = require('../common/restful');
 const { hexCrypto, generateRandomPass } = require('../common/utils');
+const { Board } = require('../models/Board');
 const { Document } = require('../models/Document');
 const { Template } = require('../models/Template');
 const { User } = require('../models/User');
 const { generateToken, ValidateToken, renewalToken } = require('../middleware/adminAuth');
+const { makeFolder } = require('../common/utils');
 const fs = require('fs');
-
-const saltRounds = 10;
+const path = require('path');
 
 const java = require('java');
 const jarFilePath1 = __dirname+'/../lib/INICrypto_v4.0.12.jar';
@@ -27,21 +29,40 @@ java.classpath.push(jarFilePath5);
 java.classpath.push(jarFilePath6);
 java.classpath.push(jarFilePath7);
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log('req.body.path : ' + req.body.path);
+    console.log('req.body.isLast : ' + req.body.isLast);
+      
+    if (req.body.path) {
+      var newDir = config.storageDIR + req.body.path;
+      console.log('newDir : ' + newDir);
+      makeFolder(newDir);
+      cb(null, newDir);
+    } else {
+      cb(null, config.storageDIR + 'temp/');
+    }
+  },
+  filename: (req, file, cb) => {
+      cb(null, file.originalname);
+  }
+});
+const upload = multer({storage});
+
 // -- 로그인/아웃 --
 // /api/admin/auth        (GET)
 // /api/admin/sso
 // /api/admin/refresh
 // /api/admin/login
 // /api/admin/logout
-// -- 사용자 관리 --
+// -- 사용자/부서 관리 --
 // /api/admin/user/list
 // /api/admin/user/info
 // /api/admin/user/update (정보 변경)
 // /api/admin/user/push   (알림 점검)
 // /api/admin/user/sync   (연계 수동)
-// -- 부서 관리 --
-// /api/admin/org/list
-// /api/admin/org/info
+// /api/admin/org/list    (미사용)
+// /api/admin/org/info    (미사용)
 // /api/admin/org/sync    (연계 수동)
 // -- 문서 관리 --
 // /api/admin/document/list
@@ -51,7 +72,16 @@ java.classpath.push(jarFilePath7);
 // /api/admin/templates/info
 // /api/admin/templates/insert
 // /api/admin/templates/delete
+// /api/admin/templates/upload
+// -- 게시판 관리 --
+// /api/admin/board/list
+// /api/admin/board/detail
+// /api/admin/board/insert
+// /api/admin/board/update
+// /api/admin/board/delete
+// /api/admin/board/attach
 
+// 시스템 연계 확인용
 router.post('/ipronet', (req, res) => {
   // restful.callOrgAPI();
   // restful.callUserAPI();
@@ -61,10 +91,13 @@ router.post('/ipronet', (req, res) => {
   // DocuUtil.unpackagingSync('C:/Users/NHIT_LSW/Desktop/', 'MiNe.xlsx', 'C:/Users/NHIT_LSW/Desktop/TEST.xlsx');
 });
 
+
+
+///////////////////////////////////////////////////////////////////////// 계정 인증 서비스 시작 /////////////////////////////////////////////////////////////////////////
 // 인증 여부 확인
 router.get('/auth', ValidateToken, (req, res) => {
   var uid = req.body.systemId;
-  User.findOne({ _id: uid }, (err, user) => {
+  User.findOne({ '_id': uid }, (err, user) => {
     if (err) return res.json({ success: false, error: err });
     if (!user) {
       return res.json({
@@ -100,15 +133,15 @@ router.post('/sso', (req, res) => {
   }
   console.log('uid: '+ uid);
 
-  User.findOne({ uid: uid }, (err, user) => {
+  User.findOne({ 'uid': uid }, (err, user) => {
     if (err) return res.json({ success: false, error: err });
     if (user) {
       // 토큰 생성
       var {accessToken, refreshToken} = generateToken(user);
-      User.updateOne({ _id: user._id }, {adminJWT: refreshToken}, (err, result) => {
+      User.updateOne({ '_id': user._id }, {'adminJWT': refreshToken}, (err, result) => {
         if (err) return res.json({ success: false, message: err });
         console.log(result);
-        res.cookie('__aToken__', accessToken,  { httpOnly: true, maxAge: 60*60*1000 });
+        res.cookie('__aToken__', accessToken,  { httpOnly: true, maxAge: 12*60*60*1000 });
         res.status(200).json({
           success: true,
           user: {
@@ -132,11 +165,11 @@ router.post('/refresh', renewalToken, (req, res) => {
   var _id = req.body.systemId;
   var _tk = req.body.accessTk;
   console.log(_tk);
-  User.findOne({ _id: _id, adminJWT: req.headers['refresh-token'] }, (err, user) => {
+  User.findOne({ '_id': _id, 'adminJWT': req.headers['refresh-token'] }, (err, user) => {
     // console.log(user);
     if (err) return res.json({ success: false, error: err });
     if (!user) return res.json({ success: false, message: 'No User!!' });
-    res.cookie('__aToken__', _tk,  { httpOnly: true, maxAge: 60*60*1000 });
+    res.cookie('__aToken__', _tk,  { httpOnly: true, maxAge: 12*60*60*1000 });
     res.status(200).json({
       success: true,
       user: {
@@ -167,7 +200,7 @@ router.post('/login', (req, res) => {
   console.log('uid: '+ uid);
 
   // 사용자 검색 (사번 또는 이메일)
-  User.findOne({ uid: uid }, (err, user) => {
+  User.findOne({ 'uid': uid }, (err, user) => {
     if (err) return res.json({ success: false, error: err });
     if (!user || user.role !== 1) {
       return res.json({
@@ -186,10 +219,10 @@ router.post('/login', (req, res) => {
       var {accessToken, refreshToken} = generateToken(user);
       
       // Refresh 토큰 DB 저장
-      User.updateOne({ _id: user._id }, {adminJWT: refreshToken}, (err, result) => {
+      User.updateOne({ '_id': user._id }, {'adminJWT': refreshToken}, (err, result) => {
         if (err) return res.json({ success: false, message: err });
         console.log(result);
-        res.cookie('__aToken__', accessToken,  { httpOnly: true, maxAge: 60*60*1000 });
+        res.cookie('__aToken__', accessToken,  { httpOnly: true, maxAge: 12*60*60*1000 });
         res.status(200).json({
           success: true,
           user: {
@@ -208,19 +241,23 @@ router.post('/login', (req, res) => {
 
 // 로그아웃
 router.post('/logout', ValidateToken, (req, res) => {
-  User.findOneAndUpdate({ _id: req.body._id }, { adminJWT: '' }, (err) => {
+  User.findOneAndUpdate({ '_id': req.body._id }, { 'adminJWT': '' }, (err) => {
     if (err) return res.json({ success: false, err });
     res.cookie('__aToken__', '__aToken__', { httpOnly: true, maxAge: 0 });
     return res.status(200).send({success: true});
   });
 });
+///////////////////////////////////////////////////////////////////////// 계정 인증 서비스 종료 /////////////////////////////////////////////////////////////////////////
 
+
+
+///////////////////////////////////////////////////////////////////////// 사용자/부서 관리 시작 /////////////////////////////////////////////////////////////////////////
 // 사용자 관리 > 목록
 router.post('/user/list', ValidateToken, async (req, res) => {
 console.log(req.cookies);
   // 페이징 처리
-  const current = req.body.pagination.current;
-  const pageSize = req.body.pagination.pageSize;
+  var current = req.body.pagination.current;
+  var pageSize = req.body.pagination.pageSize;
   var start = 0;
   if (current > 1) {
     start = (current - 1) * pageSize;
@@ -242,14 +279,14 @@ console.log(req.cookies);
   // 단어검색 
   var searchStr = {};
   var condition = [];
-
+  
   // 사용자
-  if (req.body.name) {
+  if (req.body.name && req.body.name != '') {
     condition.push({'name': new RegExp(req.body.name[0], 'i')});
   }
 
   // 부서명
-  if (req.body.org) {
+  if (req.body.org && req.body.org != '') {
     condition.push({'orgInfo.DEPART_NAME': new RegExp(req.body.org[0], 'i')});
   }
 
@@ -302,7 +339,7 @@ router.post('/user/info', ValidateToken, (req, res) => {
 
 // 사용자 관리 > 변경(정보)
 router.post('/user/update', ValidateToken, async (req, res) => {
-  User.findOne({ _id: req.body.i }).then((user, err) => {
+  User.findOne({ '_id': req.body.i }).then((user, err) => {
     if (err) return res.json({ success: false, err });
     if (req.body.k == 'init') {var pwd = generateRandomPass(); user.password = pwd;}
     if (req.body.k == 'flag') user.use = !user.use;
@@ -316,15 +353,15 @@ router.post('/user/update', ValidateToken, async (req, res) => {
 });
 
 // 사용자 관리 > 알림(점검)
-router.post('/user/sendPush', (req, res) => {
+router.post('/user/sendPush', ValidateToken, (req, res) => {
   restful.callNotify(req.body.sendInfo, req.body.recvInfo, req.body.title, req.body.content);
   return res.json({success: true,  message: '호출 성공'});
 });
 
 // 사용자 관리 > 연계 호출
 router.post('/user/sync', ValidateToken, async (req, res) => {
-  let result = await restful.callUserAPI();
-  if (result && result.status && result.status === 200) {
+  var result = await restful.callUserAPI();
+  if (result && result.status && result.status == 200) {
     return res.json({ success: true });
   } else {
     return res.json({ success: false });
@@ -333,19 +370,23 @@ router.post('/user/sync', ValidateToken, async (req, res) => {
 
 // 부서 관리 > 연계 호출
 router.post('/org/sync', ValidateToken, async (req, res) => {
-  let result = await restful.callOrgAPI();
-  if (result && result.status && result.status === 200) {
+  var result = await restful.callOrgAPI();
+  if (result && result.status && result.status == 200) {
     return res.json({ success: true });
   } else {
     return res.json({ success: false });
   }
 });
+///////////////////////////////////////////////////////////////////////// 사용자/부서 관리 종료 /////////////////////////////////////////////////////////////////////////
 
+
+
+///////////////////////////////////////////////////////////////////////// 문서 관리 서비스 시작 /////////////////////////////////////////////////////////////////////////
 // 문서 관리 > 목록
 router.post('/document/list', ValidateToken, async (req, res) => {
   // 페이징 처리
-  const current = req.body.pagination.current;
-  const pageSize = req.body.pagination.pageSize;
+  var current = req.body.pagination.current;
+  var pageSize = req.body.pagination.pageSize;
   var start = 0;
   if (current > 1) {
     start = (current - 1) * pageSize;
@@ -377,7 +418,7 @@ router.post('/document/list', ValidateToken, async (req, res) => {
   if (req.body.name) {
     var userIds = [];
     var regex = new RegExp(req.body.name[0], 'i');
-    var dataList = await User.find({ name: regex }).exec();
+    var dataList = await User.find({ 'name': regex }).exec();
     for (var idx in dataList) {
       userIds.push(dataList[idx]['_id']);
     }
@@ -413,13 +454,17 @@ router.post('/document/list', ValidateToken, async (req, res) => {
 router.post('/document/info', ValidateToken, (req, res) => {
   return res.json({ success: true, message: '/document/info' });
 });
+///////////////////////////////////////////////////////////////////////// 문서 관리 서비스 종료 /////////////////////////////////////////////////////////////////////////
 
+
+
+///////////////////////////////////////////////////////////////////////// 템플릿 문서 관리 시작 /////////////////////////////////////////////////////////////////////////
 // 템플릿 관리 > 목록
 router.post('/templates/list', ValidateToken, async (req, res) => {
 
   // 페이징 처리
-  const current = req.body.pagination.current;
-  const pageSize = req.body.pagination.pageSize;
+  var current = req.body.pagination.current;
+  var pageSize = req.body.pagination.pageSize;
   var start = 0;
   if (current > 1) {
     start = (current - 1) * pageSize;
@@ -451,7 +496,7 @@ router.post('/templates/list', ValidateToken, async (req, res) => {
   if (req.body.name) {
     var userIds = [];
     var regex = new RegExp(req.body.name[0], 'i');
-    var dataList = await User.find({ name: regex }).exec();
+    var dataList = await User.find({ 'name': regex }).exec();
     for (var idx in dataList) {
       userIds.push(dataList[idx]['_id']);
     }
@@ -488,16 +533,29 @@ router.post('/templates/info', ValidateToken, (req, res) => {
 
 // 템플릿 관리 > 등록
 router.post('/templates/insert', ValidateToken, (req, res) => {
-  if (!req.body.user) {
-    return res.json({ success: false, message: 'input value not enough!' });
-  }
-console.log(req.body);
-  const template = new Template(req.body);
-  // console.log(template);
-  template.save((err, documentInfo) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).json({
-      success: true
+  
+  if (!req.body.user || !req.body.docRef) return res.json({ success: false, message: 'input value not enough!' });
+  
+  var origin = req.body.docRef;
+  var target = req.body.docRef.replace('temp', 'templates');
+  
+  // File Access
+  fs.access(origin, fs.constants.F_OK, (err) => {
+    if (err) return console.log('not found origin file!');
+    var dirName = path.dirname(target);
+    makeFolder(dirName);
+    
+    // File Move
+    fs.rename(origin, target, (err) => {
+      if (err) return console.log('failed move file!');
+      
+      // DB Save
+      req.body.docRef = target;
+      var template = new Template(req.body);
+      template.save((err) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({ success: true });
+      });
     });
   });
 });
@@ -508,7 +566,7 @@ router.post('/templates/delete', ValidateToken, (req, res) => {
     return res.json({ success: false, message: 'input value not enough!' });
   }
 
-  const _ids = req.body._ids;
+  var _ids = req.body._ids;
 
   // 스토리지 파일 삭제
   Template
@@ -516,21 +574,187 @@ router.post('/templates/delete', ValidateToken, (req, res) => {
     .exec((err, rows) => {
       rows.forEach(template => {
         console.log(template.docRef);
-        fs.unlink(config.storageDIR + template.docRef, function (err) {
-          if (err) {
-            console.error(err);
-            return res.json({ success: false, err });
-          }
-          console.log('File has been Deleted');
+        fs.unlink(template.docRef, function (err) {
+          if (err) console.error(err);
         });
       });
 
       // DB 삭제
-      Template.deleteMany({ _id: { $in: _ids } }, function (err) {
+      Template.deleteMany({ '_id': { $in: _ids } }, function (err) {
         if (err) { return res.json({ success: false, err }); }
         return res.status(200).json({ success: true });
       });
     });
-})
+});
+
+// 템플릿 관리 > 업로드(임시파일)
+router.post('/templates/upload', ValidateToken, upload.single('file'), async (req, res) => {
+  if (req.file) {
+      await restful.callDRMUnpackaging(req.file.destination, req.file.originalname);
+      return res.json({ success: true, file: req.file });
+  } else {
+      return res.json({ success: false, message: 'file upload failed'});
+  }
+  
+});
+///////////////////////////////////////////////////////////////////////// 템플릿 문서 관리 종료 /////////////////////////////////////////////////////////////////////////
+
+
+
+///////////////////////////////////////////////////////////////////////// 게시판 정보 관리 시작 /////////////////////////////////////////////////////////////////////////
+// 게시판 관리 > 목록
+router.post('/board/list', ValidateToken, async (req, res) => {
+
+  // 페이징 처리
+  var current = req.body.pagination.current;
+  var pageSize = req.body.pagination.pageSize;
+  var start = 0;
+  if (current > 1) {
+    start = (current - 1) * pageSize;
+  }
+
+  var order = 'registeredTime';
+  var dir = 'desc';
+  if (req.body.sortField) {
+    order = req.body.sortField;
+  }
+  if (req.body.sortOrder) {
+    if (req.body.sortOrder == 'ascend') {
+      dir = 'asc';
+    } else {
+      dir = 'desc';
+    }
+  }
+
+  // 단어검색 
+  var searchStr = {};
+
+  // 템플릿 이름
+  if (req.body.title) {
+    var regex = new RegExp(req.body.title[0], 'i');
+    searchStr['title'] = new RegExp(regex, 'i');
+  }
+
+  // 생성자
+  if (req.body.name) {
+    var userIds = [];
+    var regex = new RegExp(req.body.name[0], 'i');
+    var dataList = await User.find({ 'name': regex }).exec();
+    for (var idx in dataList) {
+      userIds.push(dataList[idx]['_id']);
+    }
+    searchStr['user'] = { $in: userIds };
+  }
+
+  // 공통 템플릿
+  if (req.body.boardType) searchStr['boardType'] = req.body.boardType;
+
+  console.log(searchStr);
+
+  // 전체 건수
+  var totalCount = await Board.countDocuments(searchStr).exec();
+
+  Board
+    .find(searchStr)
+    .sort({ [order]: dir })   //asc:오름차순 desc:내림차순
+    .skip(Number(start))
+    .limit(Number(pageSize))
+    .populate({
+      path: 'user',
+      select: { name: 1, JOB_TITLE: 2 }
+    })
+    .populate({
+      path: 'comments.user', 
+      select: {name: 1, JOB_TITLE: 2}
+    })
+    .exec((err, data) => {
+      if (err) return res.json({ success: false, error: err });
+      return res.json({ success: true, boards: data, total: totalCount });
+    });
+});
+
+// 게시판 관리 > 상세
+router.post('/board/detail', ValidateToken, (req, res) => {
+  
+  if (!req.body.boardId) return res.json({ success: false, message: 'input value not enough!' });
+  
+  var boardId = req.body.boardId;
+  
+  Board.findOne({ '_id': boardId })
+  .populate({
+    path: 'user', 
+    select: {name: 1, JOB_TITLE: 2}
+  })
+  .populate({
+    path: 'comments.user', 
+    select: {name: 1, JOB_TITLE: 2, thumbnail: 3}
+  })
+  .then((board, err) => {
+    if (err) return res.json({success: false, error: err});
+    return res.json({ success: true, board: board });
+  });
+});
+
+// 게시판 관리 > 등록
+router.post('/board/insert', ValidateToken, (req, res) => {
+  if (!req.body.user || !req.body.title || !req.body.content || !req.body.boardType) {
+    return res.json({ success: false, message: 'input value not enough!' });
+  } 
+
+  var board = new Board(req.body);
+  board.save((err) => {
+    if (err) return res.json({ success: false, err });
+    return res.json({ success: true });
+  });
+});
+
+// 게시판 관리 > 수정
+router.post('/board/update', ValidateToken, (req, res) => {
+  if (!req.body.boardId || !req.body.title || !req.body.content) {
+    return res.json({ success: false, message: 'input value not enough!' });
+  }
+
+  Board.updateOne({ '_id': req.body.boardId }, {'title': req.body.title, 'content': req.body.content}, (err, result) => {
+    if (err) return res.json({ success: false, message: err });
+    return res.json({ success: true});
+  });
+});
+
+// 게시판 관리 > 삭제
+router.post('/board/delete', ValidateToken, (req, res) => {
+  if (!req.body.boardId) {
+    return res.json({ success: false, message: 'input value not enough!' });
+  }
+
+  // 스토리지 파일 삭제
+  Board
+    .find({ '_id': req.body.boardId })
+    .exec((err, board) => {
+      if (board.files.length > 0) {
+        board.files.forEach(file => {
+          console.log(file.path);
+          fs.unlink(file.path, function (err) {
+            if (err) console.error(err);
+          });
+        });
+      }
+
+      // DB 삭제
+      Board.deleteOne({ '_id': req.body.boardId }, function (err) {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({ success: true });
+      });
+    });
+});
+
+// 게시판 관리 > 첨부
+router.post('/board/attach', ValidateToken, upload.array('files'), (req, res) => {
+  if (req.files) {
+      return res.json({ success: true, files: req.files });
+  } else {
+      return res.json({ success: false, message: 'file upload failed'});
+  }
+});
+///////////////////////////////////////////////////////////////////////// 게시판 정보 관리 종료 /////////////////////////////////////////////////////////////////////////
 
 module.exports = router;
--- server/routes/openapi.js
+++ server/routes/openapi.js
@@ -3,10 +3,22 @@ const router = express.Router();
 const { User } = require('../models/User');
 const { Document } = require('../models/Document');
 
-// 서명 필요 건수 by SABUN
+// 서명 필요 건수 by SABUN (JSONP 방식-IE9 이하)
+router.get('/signCount', async (req, res) => {
+  if (!req.query.SABUN) return res.jsonp({success: false, error: 'input value not enough!'});
+  var user = await User.findOne({'SABUN': req.query.SABUN}, {'_id': 1});
+  if (!user) return res.jsonp({success: false, error: 'no user!'});
+  Document.countDocuments({ 'users': {$in:[user['_id']]}, 'signed': false, 'canceled': false, 'signedBy.user': {$ne: user['_id']} }).exec(function(err, count) {
+    if (err) return res.jsonp({success: false, error: err});
+    return res.jsonp({ success: true, count: count});
+  });
+});
+
+// 서명 필요 건수 by SABUN (JSON 방식)
 router.post('/signCount', async (req, res) => {
   if (!req.body.SABUN) return res.json({success: false, error: 'input value not enough!'});
   var user = await User.findOne({'SABUN': req.body.SABUN}, {'_id': 1});
+  if (!user) return res.json({success: false, error: 'no user!'});
   Document.countDocuments({ 'users': {$in:[user['_id']]}, 'signed': false, 'canceled': false, 'signedBy.user': {$ne: user['_id']} }).exec(function(err, count) {
     if (err) return res.json({success: false, error: err});
     return res.json({ success: true, count: count});
